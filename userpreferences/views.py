bf9c5e992454c8de702786251b417a8852fb47fb
Boluwatife Kamaldeen Lawal <bolulawal5@gmail.com>
2023-03-04 23:58:17 +0100

    preferences update

--- userpreferences/views.py
+++ userpreferences/views.py
@@ -13,24 +13,29 @@ def index(request):
     """test function
     """
     #pylint: disable=E1101
+    currency_data = []
     exists = UserPreference.objects.filter(user=request.user).exists()
     user_preferences = None
+    # get currencies json file path
+    file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
+    # pdb.set_trace()
+    # open currencies json file and append file data into python variable
+    with open(file_path, 'r',encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        for key, value in data.items():
+            currency_data.append({'name':key, 'value':value})
     if exists:
         user_preferences = UserPreference.objects.get(user=request.user)
     if request.method=="GET":
-        currency_data = []
-        file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
-        # pdb.set_trace()
-        with open(file_path, 'r',encoding="utf-8") as json_file:
-            data = json.load(json_file)
-            for key, value in data.items():
-                currency_data.append({'name':key, 'value':value})
-        return render(request,'preferences/index.html',{'currencies':currency_data})
+        return render(request,'preferences/index.html',{'currencies':currency_data,
+                                                          'user_preferences': user_preferences})
     else:
         currency = request.POST['currency']
         if exists:
             user_preferences.currency=currency
             user_preferences.save()
-        UserPreference.objects.create(user=request.user, currency=currency)
+        else:
+            UserPreference.objects.create(user=request.user, currency=currency)
         messages.success(request, 'Changes saved')
-        return render(request,'preferences/index.html',{'currencies':currency_data})
+        return render(request,'preferences/index.html',{'currencies':currency_data,
+                                                          'user_preferences': user_preferences})
